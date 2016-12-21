f69b5d0791180c52665f90c4422ef0e07ac0a30b
Student <Student@III-ITC>
2016-12-21 20:18:54 +0800

    tips

--- src/org/fagao/xxx/HelloApp.java
+++ src/org/fagao/xxx/HelloApp.java
@@ -1,7 +1,12 @@
 package org.fagao.xxx;
 
+// 呼叫方法 Import >Git >Project from Git> Clone URI
+//                >貼上複製PATH > 一直next
 
-//
+// 除錯方法  Project >右鍵 properties >JRE System..打勾
+//                 >refresh >"重點用" run as application
+                   
+
 public class HelloApp {
 
 	public static void main(String[] args) {
