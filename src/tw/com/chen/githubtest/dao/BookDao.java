d93d9951b4a451d8dd27632f87b9ffb924a807b7
KENTNONO <ck2791104@gmail.com>
2017-01-10 18:42:34 +0800

    test

--- src/tw/com/chen/githubtest/dao/BookDao.java
+++ src/tw/com/chen/githubtest/dao/BookDao.java
@@ -11,13 +11,18 @@ import java.util.Collection;
 import tw.com.chen.githubtest.entity.book.Book;
 
 public class BookDao {
+	private Connection conn;
+	private final String url = "jdbc:sqlserver://softleader.com.tw:1433;databaseName=EEIT92DB";
+	private final String user = "EEIT92";
+	private final String finding = "select * from book_chen";
+
 	public Collection<Book> findAll() {
 
 		Collection<Book> books = new ArrayList<Book>();
-		String url = "jdbc:sqlserver://softleader.com.tw:1433;databaseName=EEIT92DB";
-		String user = "EEIT92";
-		String finding = "select * from book_chen";
-		Connection conn = null;
+//		String url = "jdbc:sqlserver://softleader.com.tw:1433;databaseName=EEIT92DB";
+//		String user = "EEIT92";
+//		String finding = "select * from book_chen";
+//		Connection conn = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		try {
@@ -120,58 +125,18 @@ public class BookDao {
 		return book;
 	}
 
-	public Book findById1(Long id) {
-		Book book = null;
-		String findId = "select*from book_chen where id=?";
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			conn = DriverManager.getConnection("jdbc:sqlserver://softleader.com.tw:1433;databaseName=EEIT92DB",
-					"EEIT92", "EEIT92");
-			pstmt = conn.prepareStatement(findId);
-			pstmt.setLong(1, id);
-			rs = pstmt.executeQuery();
-
-			if (rs.next()) {
-				book = new Book();
-				book.setId(rs.getLong("id"));
-				book.setSbn_code(rs.getString("sbn_code"));
-				book.setName(rs.getString("name"));
-				book.setPrice(rs.getInt("price"));
-			}
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			try {
-				if(rs!=null){
-				rs.close();
-				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			try {
-				if(pstmt!=null){
-				pstmt.close();
-				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			try {
-				if(conn!=null){
-				conn.close();
-				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return book;
+	public Book upDate(){
+		Book result=null;
+		return result;
 	}
-
+	public int delete(){
+		int result=0;
+		return result;
+	}
+//	public Book insert(){
+//		Book result=null;
+//		conn=DriverManager.getConnection(url, user, user); 
+//		PreparedStatement pstmt = conn.prepareStatement("INSERT INTO id()");
+//		return result;
+//	}
 }
