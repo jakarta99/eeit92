e51d01742bbf7685f2002e7643957cb2f6012fb4
Student <Student@III-ITC>
2017-01-13 15:12:10 +0800

    change to UTF-8

--- src/org/fagao/xxx/HelloApp.java
+++ src/org/fagao/xxx/HelloApp.java
@@ -1,10 +1,10 @@
 package org.fagao.xxx;
 
-// �I�s��k Import >Git >Project from Git> Clone URI
-//                >�K�W�ƻsPATH > �@��next
+//呼叫方法 Import >Git >Project from Git> Clone URI
+//                >貼上複製PATH > 一直next
 
-// ������k  Project >�k�� properties >JRE System..����
-//                 >refresh >"���I��" run as application
+//除錯方法  Project >右鍵 properties >JRE System..打勾
+//                 >refresh >"重點用" run as application
                    
 
 public class HelloApp {
