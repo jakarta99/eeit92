5f257c7f5cafa1492755a3122efc69d1cd8f76dc
KENTNONO <ck2791104@gmail.com>
2017-01-17 13:25:37 +0800

    DAOUPDATE

--- src/tw/com/chen/githubtest/dao/BookDao.java
+++ src/tw/com/chen/githubtest/dao/BookDao.java
@@ -139,4 +139,129 @@ public class BookDao {
 //		PreparedStatement pstmt = conn.prepareStatement("INSERT INTO id()");
 //		return result;
 //	}
+	private static final String URL = "jdbc:sqlserver://softleader.com.tw:1433;databaseName=EEIT92DB";
+	private static final String USERNAME = "sa";
+	private static final String PASSWORD = "passw0rd";
+
+	
+
+
+
+	private static final String INSERT = "insert into book_chen (id, isbn_code, name, price) values (?, ?, ?, ?)";
+
+	public Book insert(Book bean) {
+		Book book = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			pstmt = conn.prepareStatement(INSERT);
+			pstmt.setLong(1, bean.getId());
+			pstmt.setString(2, bean.getSbn_code());
+			pstmt.setString(3, bean.getName());			
+			pstmt.setInt(4, bean.getPrice());
+			int i = pstmt.executeUpdate();
+			if (i == 1) {
+				return this.findById(bean.getId());
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+		return book;
+	}
+
+	private static final String UPDATE = "update book_chen set name=?, price=?, isbn_code=? where id=?";
+
+	public Book update(String name, Integer price,  String isbn_code, Long id) {
+		Book result = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			pstmt = conn.prepareStatement(UPDATE);
+			pstmt.setString(1, name);
+			pstmt.setInt(2, price);
+			
+			pstmt.setString(3, isbn_code);
+			pstmt.setLong(4, id);
+			int i = pstmt.executeUpdate();
+			if (i == 1) {
+				return this.findById(id);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		return result;
+	}
+
+	private static final String DELETE = "delete from book_chen where id=?";
+
+	public boolean delete(Long id) {
+		PreparedStatement pstmt=null;
+		try {
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			pstmt = conn.prepareStatement(DELETE);
+			pstmt.setLong(1, id);
+			int i = pstmt.executeUpdate();
+			if (i == 1) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				if (pstmt!=null) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (conn!=null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}	
+		}
+		return false;
+	}
+	
+	
 }
